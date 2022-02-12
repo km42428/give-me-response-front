5ef25d74bbc956804cac7366c6aaed43159a1c77
km42428 <moriyama-k@aidemy.net>
2022-02-12 13:51:46 +0900

    頷きgifの表示まで

--- index.py
+++ index.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
